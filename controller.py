174a43da8d64df51765d9bdbbad2fbd696c77244
zrp8858 <zacharypatterson789@gmail.com>
2025-08-02 14:16:32 -0400

    Added comments to the controller class.

--- controller.py
+++ controller.py
@@ -6,19 +6,22 @@ class Controller:
         self.model = model
         self.view = view
 
-        # Initialize View with model data
         # Bind button to handler
         self.view.setButtonCommand(self.updateModel)
 
     def updateModel(self):
+        # Getting input information
         newName = self.view.getNameEntry()
         newPosition = self.view.getPositionEntry()
         newPoints = self.view.getPointsEntry()
 
+        # Creating a new player object
         newPlayer = Player(newName, newPosition, newPoints)
 
+        # Adding the player to the model
         self.model.addPlayerData(newPlayer)
 
+        # Updating the view (with the first player in the model)
         self.view.setNameLabel(self.model.getPlayers()[0].getName())
         self.view.setPositionLabel(self.model.getPlayers()[0].getPosition())
         self.view.setPointsLabel(self.model.getPlayers()[0].getPoints())
